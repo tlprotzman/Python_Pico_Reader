4e5d930e4aef2c5ebc52fc73f4b42dddc1b056d7
Skagamaster <40399925+Skagamaster@users.noreply.github.com>
2021-04-13 14:19:33 -0400

    Update basic_tests.py

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -22,6 +22,20 @@ fig, ax = plt.subplots(8, 4, constrained_layout=True)
 for i in range(8):
     for j in range(4):
         index = 4 * i + j
-        ax[i][j].hist(ring_sums[i], bins)
+        ax[i][j].hist(ring_sums[i], bins, histtype='step')
         ax[i][j].set_title("Ring " + str(index + 1))
 plt.show()
+plt.close()
+
+plt.figure(figsize=(16, 9))
+index = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+# Let's just sum the outer rings.
+mip_sum = np.sum(ring_sums[index], axis=0)
+plt.hist(mip_sum, bins, histtype='step', density=True)
+plt.xlabel(r"$\Sigma_{nMIP}$", fontsize=15)
+plt.ylabel(r"$\frac{dN}{d\Sigma_{nMIP}}$", fontsize=15)
+plt.yscale('log')
+plt.title("nMIP Sums for All Outer Rings", fontsize=20)
+plt.tight_layout()
+plt.show()
+
